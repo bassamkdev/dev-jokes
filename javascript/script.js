e5758b6fd04ae2f27a458c284c7a1453b5209212
bassamkdev <bassamkazemi.dev@gmail.com>
2020-08-04 20:59:56 -0700

    implemented text to speech and joke apis

--- javascript/script.js
+++ javascript/script.js
@@ -8,9 +8,40 @@ let speechApiConfig = {
   f: "44khz_16bit_stereo",
   ssml: false,
 };
-
+const button = document.getElementById("button");
+const quote = document.getElementById("quote");
 function speaker() {
   VoiceRSS.speech(speechApiConfig);
 }
 
-speaker();
+async function getJokes() {
+  button.disabled = true;
+  let joke = "";
+  try {
+    let response = await fetch(
+      "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist"
+    );
+    let data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup}   \n${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    speechApiConfig.src = joke;
+    quote.innerText = joke;
+
+    speaker();
+  } catch (err) {
+    console.err(err);
+  }
+}
+
+const handleButtonClick = () => {
+  getJokes();
+};
+
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", () => {
+  button.disabled = false;
+  quote.innerText = "Try again !";
+});
